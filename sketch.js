7e52601a3999aacaa1727c0d230ca80697b84425
Yatharth <yatharth13091997@gmail.com>
2021-03-09 18:48:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,35 @@
+let pointarray=[[1,2]];
+let i=0;
 
+function setup()
+{
+createCanvas(windowWidth,windowHeight);
+frameRate(240);
+
+
+}
+
+function draw()
+{
+
+noStroke();
+fill(0,0);
+pointarray[i]=[mouseX,mouseY];
+strokeWeight(3);
+stroke(237, 34, 93);
+beginShape();
+
+for(j=0;j<i;j++)
+{
+
+  vertex(pointarray[j][0],pointarray[j][1]);
+  console.log(pointarray);
+}
+endShape();
+
+i++;
+
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
